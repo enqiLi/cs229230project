de3eb00bb0667dd2a39715986f27229a47a37f15
Wenqi Li <wenqili@stanford.edu>
2022-11-21 14:10:31 -0800

    Bug in unflatten image tensor

--- dataprocessing.py
+++ dataprocessing.py
@@ -65,6 +65,8 @@ def get_pad_images(path):
 def get_images_tensor(arr_images, size, flatten=True):
     if flatten:
         tensor_images = arr_images.reshape(size, -1)
+    else:
+        tensor_images = arr_images
     tensor_images = torch.from_numpy(tensor_images)
     tensor_images = tensor_images.float()
     tensor_images = 1 - tensor_images / 255
